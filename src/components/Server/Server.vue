3889cc66575408a6bd6e6fff4adc7ab693e824ce
GreywindGit <GreywindGit@users.noreply.github.com>
2019-08-21 14:53:41 +0200

    Display server list and individual statuses

---
+++ src/components/Server/Server.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="list-group-item" @click="itemClicked">Server #{{ server.id }}</li>
+</template>
+
+<script>
+    import { eventBus } from '../../main';
+
+    export default {
+        props: {
+            server: Object
+        },
+        methods: {
+            itemClicked() {
+                eventBus.$emit('itemClick', this.server);
+            }
+        }
+    };
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+        <h3 v-if="server">Server #{{ server.id }} status: '{{ server.status }}'</h3>
+        <h3 v-else>Server status is currently not displayed.</h3>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../../main";
+
+export default {
+  data: function() {
+    return {
+      server: null
+    };
+  },
+  created() {
+    eventBus.$on("itemClick", data => {
+      this.server = data;
+    });
+  }
+};
+</script>
+
+<style>
+</style>
